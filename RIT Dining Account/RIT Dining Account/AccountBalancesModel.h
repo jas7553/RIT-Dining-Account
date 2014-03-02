9950504abf6bd0ce3262c9d55d86bdad9017be47
Jason A Smith <jas7553@rit.edu>
2014-03-02 14:34:36 -0500

    Adding an AccountBalancesModel class

---
+++ RIT Dining Account/RIT Dining Account/AccountBalancesModel.h
@@ -0,0 +1,23 @@
+//
+//  AccountBalancesModel.h
+//  RIT Dining Account
+//
+//  Created by Jason A Smith on 3/2/14.
+//  Copyright (c) 2014 Jason A. Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AccountBalancesModel : NSObject
+
+- (float)tigerBucksBalance;
+
+- (float)foodDebitBalance;
+
+- (NSString *)mealPlan;
+
+- (float)mealPlanBalance;
+
+- (float)optionBalanceLabel;
+
+@end
---
+++ RIT Dining Account/RIT Dining Account/AccountBalancesModel.m
@@ -0,0 +1,42 @@
+//
+//  AccountBalancesModel.h
+//  RIT Dining Account
+//
+//  Created by Jason A Smith on 3/2/14.
+//  Copyright (c) 2014 Jason A. Smith. All rights reserved.
+//
+
+#import "AccountBalancesModel.h"
+
+@interface AccountBalancesModel ()
+
+@end
+
+@implementation AccountBalancesModel
+
+- (float)tigerBucksBalance
+{
+    return 0.0;
+}
+
+- (float)foodDebitBalance
+{
+    return 0.0;
+}
+
+- (NSString *)mealPlan
+{
+    return nil;
+}
+
+- (float)mealPlanBalance
+{
+    return 0.0;
+}
+
+- (float)optionBalanceLabel
+{
+    return 0.0;
+}
+
+@end
